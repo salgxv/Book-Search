c1ea068f3f9632873584762e1968f81775c689ab
salgxv <salvarela09@yahoo.com>
2025-04-22 23:02:52 -0700

    fixed connect

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -2,9 +2,6 @@ import mongoose from 'mongoose';
 
 const connectionString = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/booksearch';
 
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(connectionString); // ✅ no options needed
 
 export default mongoose.connection;

2d3afd10be1a2a90fe8c0d6b09f7c88dfc38f0d5
salgxv <salvarela09@yahoo.com>
2025-04-22 22:33:23 -0700

    auth.js

--- server/src/utils/auth.ts
+++ server/src/utils/auth.ts
@@ -1,23 +1,24 @@
-// import jwt from 'jsonwebtoken';
-// import { Request } from 'express';
+import jwt from 'jsonwebtoken';
+import { Request } from 'express';
 
-// const secret = 'mysecretsshh';
-// const expiration = '2h';
+const secret = process.env.JWT_SECRET || 'fallbackSecret';
+const expiration = '2h';
 
-// export const authMiddleware = ({ req }: { req: Request }) => {
-//   let token = req.headers.authorization?.split(' ').pop() || '';
-//   if (!token) return req;
+export const authMiddleware = ({ req }: { req: Request }) => {
+  let token = req.headers.authorization?.split(' ').pop() || '';
 
-//   try {
-//     const { data } = jwt.verify(token, secret) as any;
-//     return { user: data };
-//   } catch {
-//     console.log('Invalid token');
-//     return req;
-//   }
-// };
+  if (!token) return { req };
 
-// export const signToken = ({ email, _id }: { email: string; _id: string }) => {
-//   const payload = { email, _id };
-//   return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-// };
+  try {
+    const { data } = jwt.verify(token, secret) as any;
+    return { user: data };
+  } catch {
+    console.warn('Invalid token');
+    return { req };
+  }
+};
+
+export const signToken = ({ email, _id }: { email: string; _id: string }) => {
+  const payload = { email, _id };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+};
